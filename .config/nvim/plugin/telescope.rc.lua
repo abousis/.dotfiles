888942cfe563568d5f96844f8d194a3dc762f98a
abousis <25039645+abousis@users.noreply.github.com>
2023-03-12 11:52:29 +0200

    Fuzzy searching buffers.

--- .config/nvim/plugin/telescope.rc.lua
+++ .config/nvim/plugin/telescope.rc.lua
@@ -2,3 +2,4 @@ local keymap = vim.keymap
 local opts = { noremap = true }
 
 keymap.set('n', '<C-f>', '<Cmd>Telescope find_files<CR>', opts)
+keymap.set('n', '<C-b>', '<Cmd>Telescope buffers<CR>', opts)
