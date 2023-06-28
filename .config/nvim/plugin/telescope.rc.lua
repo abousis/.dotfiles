05863981d4e1c4d661524b8ceba6c4df3c7e481f
Angelos Bousis <25039645+abousis@users.noreply.github.com>
2023-06-28 18:28:31 +0300

    Avoid keymap overlapping with tmux bind-key.

--- .config/nvim/plugin/telescope.rc.lua
+++ .config/nvim/plugin/telescope.rc.lua
@@ -3,4 +3,4 @@ local opts = { noremap = true }
 
 keymap.set('n', '<C-f>', '<Cmd>Telescope find_files<CR>', opts)
 keymap.set('n', '<C-h>', '<Cmd>Telescope find_files hidden=true<CR>', opts)
-keymap.set('n', '<C-b>', '<Cmd>Telescope buffers<CR>', opts)
+keymap.set('n', '<C-m>', '<Cmd>Telescope buffers<CR>', opts)
