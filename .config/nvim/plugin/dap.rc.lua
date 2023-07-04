1ed1b52956d631feb276f504fd2774c1b05931d4
Angelos Bousis <25039645+abousis@users.noreply.github.com>
2023-07-04 20:30:57 +0300

    Debugger config for C.

--- .config/nvim/plugin/dap.rc.lua
+++ .config/nvim/plugin/dap.rc.lua
@@ -142,3 +142,35 @@ for i, js_type in ipairs(js_types) do
 		},
 	}
 end
+
+---- C debugger configuration
+dap.adapters.cppdbg = {
+	id = 'cppdbg',
+	type = 'executable',
+	command = os.getenv("HOME") .. '/.local/share/nvim/mason/bin/OpenDebugAD7',
+}
+
+dap.configurations.c = {
+	{
+		name = "Launch file",
+		type = "cppdbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+		end,
+		cwd = '${workspaceFolder}',
+		stopAtEntry = true,
+	},
+	{
+		name = 'Attach to gdbserver :1234',
+		type = 'cppdbg',
+		request = 'launch',
+		MIMode = 'gdb',
+		miDebuggerServerAddress = 'localhost:1234',
+		miDebuggerPath = '/usr/bin/gdb',
+		cwd = '${workspaceFolder}',
+		program = function()
+			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+		end,
+	},
+}
